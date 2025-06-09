b935e5f096fa982ebcdd91b1082b88bdce6960cc
Conor McGee <conor.mcgee@digital.homeoffice.gov.uk>
2025-06-09 17:00:55 +0100

    DD-679 add rollup and ESM compatibility to hmpo app for govuk frontend v5

--- example/assets/javascripts/app.js
+++ example/assets/javascripts/app.js
@@ -1,2 +1,10 @@
-/* global window */
-window.GOVUKFrontend.initAll();
+// Import all required JavaScript here.
+// This includes govuk-frontend, hmpo-components, and any custom scripts for your app.
+// Rollup will bundle everything imported in this file.
+// Add your custom JavaScript imports below as needed.
+
+import { initAll } from 'govuk-frontend';
+import 'hmpo-components/all.js';
+
+// Initialize GOV.UK Frontend
+initAll();
